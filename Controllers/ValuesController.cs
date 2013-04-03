c3f1ba45504df6ea098b8d72a51b8a33323006c4
marcin <nawrockim@op.pl>
2013-04-03 19:23:51 +0200

    Getting sample data from the server

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -7,12 +7,27 @@ using System.Web.Http;
 
 namespace WebApiPostIt.Controllers
 {
+    public class PostIt
+    {
+        public DateTime CreatedOn { get; set; }
+        public string User { get; set; }
+        public string Content { get; set; }
+        public string Subject { get; set; }
+    }
+
     public class ValuesController : ApiController
     {
+        List<PostIt> postits = new List<PostIt>() { 
+            new PostIt{CreatedOn = new DateTime(2012, 11, 11), Content= "TEST1", Subject="TEST1", User="USER"},
+            new PostIt{CreatedOn = new DateTime(2011, 11, 11), Content= "TEST2", Subject="TEST2", User="USER"},
+            new PostIt{CreatedOn = new DateTime(2014, 11, 11), Content= "TEST3", Subject="TEST3", User="USER"},
+            new PostIt{CreatedOn = new DateTime(2015, 11, 11), Content= "TEST4", Subject="TEST4", User="USER"},
+        };
+
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<PostIt> Get()
         {
-            return new string[] { "value1", "value2" };
+            return postits;
         }
 
         // GET api/values/5
